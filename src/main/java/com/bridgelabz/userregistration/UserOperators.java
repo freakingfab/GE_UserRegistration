b4b47452f3ff1ccefada7620b1dd2da6885ae50a
Daksh <dakshnahar2001@gmail.com>
2023-11-30 21:20:09 +0530

    Added: UC8 password rule 4 implemented

--- src/main/java/com/bridgelabz/userregistration/UserOperators.java
+++ src/main/java/com/bridgelabz/userregistration/UserOperators.java
@@ -60,7 +60,7 @@ public class UserOperators {
         @return: boolean
      */
     public boolean isValidPassword(String password){
-        String passwordRegex = "^(?=.*[A-Z])(?=.*\\d).{8,}$";
+        String passwordRegex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z\\d]).{8,}$";
         return password.matches(passwordRegex);
     }
 }
