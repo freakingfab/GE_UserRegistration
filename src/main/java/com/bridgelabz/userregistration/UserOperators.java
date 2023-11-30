0cb91256cd1edacc4291e384ef5f14ca64299c35
Daksh <dakshnahar2001@gmail.com>
2023-11-30 21:15:10 +0530

    Added: UC5 password rule 2 implemented

--- src/main/java/com/bridgelabz/userregistration/UserOperators.java
+++ src/main/java/com/bridgelabz/userregistration/UserOperators.java
@@ -60,7 +60,7 @@ public class UserOperators {
         @return: boolean
      */
     public boolean isValidPassword(String password){
-        String passwordRegex = "^.{8,}$";
+        String passwordRegex = "^(?=.*[A-Z]).{8,}$";
         return password.matches(passwordRegex);
     }
 }
